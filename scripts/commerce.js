e12f846e1b169d042bd5136e8524a545d10045f9
Papilion Dániel <daniel.papilion@bluprintx.com>
2025-10-20 18:54:35 +0200

    Fetch real category data

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -273,13 +273,38 @@ export function mapProductAcdl(product) {
   };
 }
 
-window.categoryData = window.categoryData || fetch(`${window.origin}/data/data.json`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok ${response.statusText}`);
+async function fetchCategoryTree() {
+  const query = `
+    query categoryTree {
+      categories {
+        items {
+          name
+          url: url_key
+          id
+          children {
+            name
+            url: url_key
+            id
+            children {
+              name
+              url: url_key
+              id
+              children {
+                name
+                url: url_key
+                id
+              }
+            }
+          }
+        }
+      }
     }
-    return response.json();
-  });
+  `;
+  const categoryData = await performCatalogServiceQuery(query);
+  return { data: categoryData };
+}
+
+window.categoryData = window.categoryData || fetchCategoryTree();
 
 export function getProperty(items, propertyName, searchCriteria) {
   const stack = [...items];
